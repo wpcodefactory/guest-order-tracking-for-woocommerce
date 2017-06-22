ce4f7610d4c4a2361aade68bdcfa78d418ad4b4b
pablo-pacheco <pablo.pacheco@origgami.com.br>
2017-06-22 19:12:07 -0300

    Fix action links

--- classes/admin/alg-gotwc-admin-general.php
+++ classes/admin/alg-gotwc-admin-general.php
@@ -19,7 +19,7 @@ if ( ! class_exists( 'Alg_GOTWC_Admin_General' ) ) {
 		 * @since   1.0.0
 		 */
 		public function action_links( $links ) {
-			$admin_tab = self::get_admin_settings();
+			$admin_tab = Alg_GOTWC_Core::get_admin_tab();
 			$custom_links = array( '<a href="' . admin_url( 'admin.php?page=wc-settings&tab='.$admin_tab->admin_tab_id.'' ) . '">' . __( 'Settings', 'guest-order-tracking-for-woocommerce' ) . '</a>' );
 			return array_merge( $custom_links, $links );
 		}
