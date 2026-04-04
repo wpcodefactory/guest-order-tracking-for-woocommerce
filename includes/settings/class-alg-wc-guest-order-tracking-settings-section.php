9fd5f2f1f9fb1b3939fa8e3ad99d0e4ab2190c6b
Istiaq Nirab <40653908+Ok9xNirab@users.noreply.github.com>
2026-04-04 20:40:31 +0600

    update @version and readme.

--- includes/settings/class-alg-wc-guest-order-tracking-settings-section.php
+++ includes/settings/class-alg-wc-guest-order-tracking-settings-section.php
@@ -2,7 +2,7 @@
 /**
  * Guest Order Tracking for WooCommerce - Section Settings
  *
- * @version 2.0.0
+ * @version 2.1.8
  * @since   2.0.0
  *
  * @author  WPFactory
@@ -13,14 +13,22 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 if ( ! class_exists( 'Alg_WC_Guest_Order_Tracking_Settings_Section' ) ) :
 
 class Alg_WC_Guest_Order_Tracking_Settings_Section {
-	
+
 	/**
-	 * FIX: PHP 8.2+ এ dynamic property deprecated।
+	 * Id.
 	 *
 	 * @version 2.1.8
+	 * @since   2.1.8
 	*/
-	public $id = '';   
-    public $desc = ''; 
+	public $id = '';
+
+	/**
+	 * Desc.
+	 *
+	 * @version 2.1.8
+	 * @since   2.1.8
+	 */
+    public $desc = '';
 
 	/**
 	 * Constructor.
